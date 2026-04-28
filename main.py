431e9433ecacc74b5cc5799cebd88739a86d495f
Tim Robinson <tim.robinson@lisd.us>
2026-04-28 17:01:03 -0400

    Converted time format

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 '''
 TODO:
-    1. Reformat the time 
     2. Allow user to enter a time for timer
     3. Create a notifciation to user when timer time is elapsed
     4. Allow timer app to be paused and resumed
+    5. Incorporate adding a work task to timer for visual display. 
 '''
 
 from tkinter import *
@@ -13,7 +13,7 @@ import time
 def update_labels():
     elp_time = time.perf_counter() - str_time
     timer_label.config(text=f'Seconds: {elp_time // 1:.0f}')
-    clock_label.config(text=time.strftime('%H:%M:%S'))
+    clock_label.config(text=time.strftime('%I:%M:%S %p', time.localtime()))
     root.after(50, update_labels) # test that this delay or debounce here does not effect time accuracy. 
 
 def clear_timer():
@@ -26,9 +26,17 @@ def set_time():
     set_time_wind.title("Enter Time")
     set_time_wind.geometry("100x100")
 
-    ttk.Label(set_time_wind, text="testing new window").pack(pady=15)
+    ttk.Label(set_time_wind, text="Editing time").pack(pady=15)
     ttk.Button(set_time_wind, text='\u2191').pack(pady=5)
     ttk.Button(set_time_wind, text="Finish", command=set_time_wind.destroy).pack(pady=15)
+
+def draw_clock():
+    # draw the clock gui and update image based on current time
+    canvas_dims = 150, 150
+    clock_canvas = Canvas(tab_clock, width=150, height=150, bg="white")
+    clock_canvas.pack()
+    # render image of clock here.
+
 
 # root init
 root = Tk() 
@@ -63,5 +71,6 @@ ttk.Button(tab_timer, text="Clear Timer ❌", command=clear_timer).pack(pady=15)
 ttk.Button(root, text="EXIT App", command=root.destroy).pack(pady=10)
 
 # update and loop 
+draw_clock()
 update_labels()
 root.mainloop()
